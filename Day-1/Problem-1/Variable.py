bebf3357a86126bc3c912ec13d0170552387bce7
sourav louha <souravlouha00@gail.com>
2024-04-10 15:39:31 +0530

    file change

---
+++ Day-1/Problem-1/Variable.py
@@ -0,0 +1,11 @@
+vname= 'Soumya'
+vage= 10
+vgender= 'm'
+vweight= 72.17
+
+print("Name is:",vname)
+print("Age is:",vage)
+print("Gender is:",vgender)
+print("Weight is:",vweight)
+
+print("Name is :",vname,"Age is :",vage,"Gender is :",vgender,"Weight is :",vweight)
